9f0318aeed984ea4d96e30137dc6297b9210e531
George Burton <burtonageo@gmail.com>
2015-11-17 20:16:55 +0000

    Hello world!

---
+++ README.md
@@ -0,0 +1,2 @@
+# gpu_mandlebrot
+Calculates mandlebrot on the GPU and displays it in an SDL window
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+extern crate opencl;
+extern crate sdl2;
+
+fn main() {
+    println!("Hello, world!");
+}
