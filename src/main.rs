e4232c8da00a74c52c3e9d777029b06bee1a9410
George Burton <burtonageo@gmail.com>
2015-11-18 09:14:36 +0000

    Add simple OpenCL example

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+//! Doc placeholder
+
 extern crate opencl;
 extern crate sdl2;
 
-use opencl::mem::CLBuffer;
 use sdl2::pixels::Color;
 use sdl2::keyboard::Keycode;
 
@@ -15,7 +16,37 @@ __kernel void multiply_by_scalar(__global float const* const src_buf,
 ";
 
 fn main() {
-    let (device, ctx, queue) = opencl::util::create_compute_context().expect("Could not initialise OpenCL");
+    // Initial kernel stuff
+    {
+        let (device, ctx, queue) = opencl::util::create_compute_context().expect("Could not initialise OpenCL");
+        const COEFF: f32 = 5.4321;
+
+        let vec_src = vec![1.0f32, 2.0, 3.0, 4.0, 5.0];
+
+        let cl_src_buf = ctx.create_buffer::<f32>(vec_src.len(), opencl::cl::CL_MEM_READ_ONLY);
+        let cl_dst_buf = ctx.create_buffer::<f32>(vec_src.len(), opencl::cl::CL_MEM_WRITE_ONLY);
+
+        queue.write(&cl_src_buf, &&vec_src[..], ());
+
+        let program = {
+            let p = ctx.create_program_from_source(KERNEL);
+            p.build(&device).expect("Could not build program");
+            p
+        };
+
+        let kernel = program.create_kernel("multiply_by_scalar");
+        kernel.set_arg(0, &cl_src_buf);
+        kernel.set_arg(1, &COEFF);
+        kernel.set_arg(2, &cl_dst_buf);
+
+        let event = queue.enqueue_async_kernel(&kernel, vec_src.len(), None, ());
+
+        let vec_dst: Vec<f32> = queue.get(&cl_dst_buf, &event);
+
+        println!("  {:?}", &vec_src[..]);
+        println!("+ {:?}", COEFF);
+        println!("= {:?}", &vec_dst[..]);
+    }
 
     let sdl = sdl2::init().expect("Could not intialise SDL library");
     let mut renderer = sdl.video()
